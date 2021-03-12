3f29f0c092ceb849fc5a5b0786e6887a40b33758
reych-labs <57446406+reych-labs@users.noreply.github.com>
2021-03-12 15:29:30 +0100

    Afegits components i listeners

--- src/cat/mvm/graphics/MyFrame.java
+++ src/cat/mvm/graphics/MyFrame.java
@@ -1,7 +1,11 @@
 package cat.mvm.graphics;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MyFrame extends JFrame {
     public MyFrame(){
@@ -23,6 +27,42 @@ public class MyFrame extends JFrame {
         this.setTitle("La meva finestra");
         Image myIcon = screen.getImage("src/cat/mvm/graphics/icon.gif");
         setIconImage(myIcon);
+
+        //Creem els botons
+        JPanel buttons = new JPanel();
+        JButton hello = new JButton("Digues Hola");
+        JButton bye = new JButton("Digues Adeu");
+        buttons.add(hello);
+        buttons.add(bye);
+
+        //Creem el camp de text
+        JTextField txtOut = new JTextField("Pulsa un botó");
+
+        //Afegim els listeners als components
+        hello.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent ev) {
+                        txtOut.setText("Hola!");
+
+                    }});
+        bye.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent ev) {
+                        txtOut.setText("Adeu!!!!");
+
+                    }});
+
+        JSplitPane mainPanel =
+                new JSplitPane(JSplitPane.VERTICAL_SPLIT,buttons,txtOut);
+        getContentPane().add(mainPanel);
+
+        addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent ev) {
+                        System.exit(0);
+                    }
+                });
+
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
